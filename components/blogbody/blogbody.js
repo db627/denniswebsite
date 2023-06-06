59a1698eda91483a650dccf12b5e277a0d3904fb
db627 <db627@njit.edu>
2023-06-06 17:03:05 -0400

    fix body errors

--- components/blogbody/blogbody.js
+++ components/blogbody/blogbody.js
@@ -1,8 +1,9 @@
 export default function Blogbody(){
     return(
+        <div  style={{height:"50em"}}>
         <div className="flex justify-center pl-5">
             <h1 className="text-center text-white">Blog Coming Soon!</h1>
             <button className="bg-teal-400 rounded-lg px-2 text-center justify-center"><a href="/">Back To Home</a></button>
-        </div> 
+        </div> </div>
     )
 }
