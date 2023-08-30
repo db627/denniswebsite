4e4d0f99adc1a13ac296db33610229ae0a67bd38
Dennis Boguslavskiy <db627@njit.edu>
2023-08-30 13:13:58 -0400

    update project links

--- components/skills/projects.js
+++ components/skills/projects.js
@@ -5,23 +5,23 @@ const projectList = [
   {
     name: "Personal Website",
     description:
-      "A distinctive platform designed to showcase my diverse projects. This website is intricately crafted using Next.js, augmented with the real-time capabilities of Firebase. For styling, I've leveraged the power of Tailwind CSS, and to add interactive animations, I utilized Framer Motion. A blend of these advanced technologies culminates in a seamless user experience..",
-    skills: "Next.js, Tailwind CSS, Firebase, Framer Motion",
-    githubLink: "#github-link-for-personal-website",
+      "A distinctive platform designed to showcase my diverse projects. This website is intricately crafted using Next.js, augmented with the real-time capabilities of Firebase. For styling, I've leveraged the power of Tailwind CSS, and to add interactive animations, I utilized Framer Motion. A blend of these advanced technologies culminates in a seamless user experience.",
+    skills: "Next.js, Tailwind CSS, Firebase, Framer Motion, GitHub",
+    githubLink: "https://dennisboguslavskiy.com/",
   },
   {
     name: "Blog Website",
     description:
-      "A dynamic blogging platform where I articulate my insights and musings. Built on the robust framework of Next.js, the site enjoys a responsive design made possible with Bootstrap 5. Every article and interface detail resonates with my commitment to providing valuable content in a visually appealing manner..",
-    skills: "Next.js, Bootstarp 5",
-    githubLink: "#github-link-for-blog-website",
+      "Worked in a team to develop a blogging website that uses Next.js to render pages and Bootstrap 5 for styling. The website is hosted on GitHub Pages and is powered by the GitHub API. The website is fully responsive. The website is also integrated with Google Analytics for tracking. I used GitHub Actions to automate the deployment process.",
+    skills: "Next.js, Bootstarp 5, GitHub, Git Collaboration, GitHub Actions",
+    githubLink: "https://njit-wis.github.io/project-2-team-dennis-and-paul/",
   },
   {
     name: "RobinPy",
     description:
       "A stock-monitoring tool that stands as a testament to automation's power. Integrating Robinhood via the RobinStocks API, this Python-based application fetches daily standings of my stock portfolio. Moreover, through the Twilio API, it ensures I stay updated by sending daily predictions and standings for each stock directly to my phone. RobinPy simplifies stock tracking, making financial insights more accessible.",
     skills: "Python, RobinStocks API, Twilio API",
-    githubLink: "#github-link-for-robinpy",
+    githubLink: "",
   },
 ];
 
@@ -71,7 +71,7 @@ const Projects = () => {
               href={project.githubLink}
               className="mt-2 text-yellow-400 text-semibold hover:underline"
             >
-              Github
+              Github Link
             </a>
           </motion.div>
         ))}
