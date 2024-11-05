1b1817b4afdc6422673bd982b655b4213c96f1d9
Dennis Boguslavskiy <db627@njit.edu>
2024-11-05 18:43:47 -0500

    cool scroll

--- components/sidenav.js
+++ components/sidenav.js
@@ -1,37 +1,32 @@
 // SideNav.jsx
 import { FaHome, FaUser, FaBriefcase, FaProjectDiagram, FaCogs, FaFilePdf } from 'react-icons/fa';
-import { useState } from 'react';
+import { Link } from 'react-scroll';
 
 function SideNav() {
-  const [hovered, setHovered] = useState(null);
-
   return (
-    <nav className="fixed top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 bg-opacity-75 w-14 h-auto py-4 rounded-full flex flex-col items-center shadow-lg hidden md:flex"> {/* 'hidden md:flex' hides it on mobile */}
-      <NavItem href="#hero" icon={<FaHome />} label="Home" hovered={hovered} setHovered={setHovered} />
-      <NavItem href="#experience" icon={<FaBriefcase />} label="Experience" hovered={hovered} setHovered={setHovered} />
-      <NavItem href="#about" icon={<FaUser />} label="About" hovered={hovered} setHovered={setHovered} />
-      <NavItem href="#projects" icon={<FaProjectDiagram />} label="Projects" hovered={hovered} setHovered={setHovered} />
-      <NavItem href="#skills" icon={<FaCogs />} label="Skills" hovered={hovered} setHovered={setHovered} />
-      <NavItem href="/resume" icon={<FaFilePdf />} label="Resume" hovered={hovered} setHovered={setHovered} />
+    <nav className="fixed top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 bg-opacity-75 w-14 h-auto py-4 rounded-full flex-col items-center shadow-lg hidden md:flex">
+      <NavItem to="hero" icon={<FaHome />} label="Home" />
+      <NavItem to="experience" icon={<FaBriefcase />} label="Experience" />
+      <NavItem to="about" icon={<FaUser />} label="About" />
+      <NavItem to="projects" icon={<FaProjectDiagram />} label="Projects" />
+      <NavItem to="skills" icon={<FaCogs />} label="Skills" />
+      <NavItem href="/resume" icon={<FaFilePdf />} label="Resume" />
     </nav>
   );
 }
 
-function NavItem({ href, icon, label, hovered, setHovered }) {
+function NavItem({ to, icon, label }) {
   return (
-    <a
-      href={href}
-      onMouseEnter={() => setHovered(label)}
-      onMouseLeave={() => setHovered(null)}
-      className="relative group mb-6 text-gray-400 hover:text-white transition-colors duration-300 p-2"
+    <Link
+      to={to}
+      smooth={true}
+      duration={500}
+      offset={-70} // Adjust offset for fixed header
+      className="text-gray-400 hover:text-white transition-colors duration-300 p-2"
     >
       {icon}
-      {hovered === label && (
-        <span className="absolute left-12 top-1/2 transform -translate-y-1/2 bg-gray-900 text-white text-xs py-1 px-2 rounded-md shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-          {label}
-        </span>
-      )}
-    </a>
+      <span className="sr-only">{label}</span> {/* For accessibility */}
+    </Link>
   );
 }
 
